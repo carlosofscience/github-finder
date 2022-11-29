50b1ace131850c15b78cf04b0bad934b171c0a25
carlosofscience <carlosofscience@gmail.com>
2022-11-28 20:01:54 -0500

    setting up react router and env variables

---
+++ src/Pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        React App to search Github profiles and see profile details. This project is part of the Udemy Course by Brad Traversy.
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+    </div>
+  )
+}
+
+export default About
